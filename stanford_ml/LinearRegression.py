3ef772d04ae8d2e797fd64a6bf66869bd9f3db79
siqwu <siqwu@microstrategy.com>
2016-04-14 14:59:36 +0800

    refine figure maker

--- stanford_ml/LinearRegression.py
+++ stanford_ml/LinearRegression.py
@@ -5,61 +5,64 @@ from sklearn import linear_model
 # Machine Learning by Andrew Ng on Coursera week 1
 # Linear Regression with one variable
 
+
 def main():
     # Load the dataset
     dataset = loadtxt('datasets/ex1data1.txt', delimiter=',')
+    m = dataset.shape[0]
+    X = dataset[:, 0].reshape(m, 1)
+    y = dataset[:, 1].reshape(m, 1)
 
     # Prepare the canvas
-    figure(figsize=(12, 7), dpi=100)
+    figure(figsize=(14, 10), dpi=100)
+    # Hypothesis by gradient descent
     p1 = subplot(221)
+    # Hypothesis by sklearn linear regression
     p2 = subplot(222)
+    # Monitor of cost function value
     p3 = subplot(223)
+    # Contour figure with respect to theta_0 and theta_1
     p4 = subplot(224)
 
-    (m, n) = dataset.shape
-    X = dataset[:, :n-1].reshape(m, n-1)
-    y = dataset[:, n-1].reshape(m, 1)
-
-    # Plot the dataset
-    p1.scatter(X, y, marker='x', c='r')
-    p1.set_title('Profit distribution')
-    p1.set_xlabel('Population of City in 10,000s')
-    p1.set_ylabel('Profit in $10,000s')
-
-    p2.scatter(X, y, marker='x', c='r')
-    p2.set_title('Fit by sklearn linear regression')
-    p2.set_xlabel('Population of City in 10,000s')
-    p2.set_ylabel('Profit in $10,000s')
+    # Plot the scatter points
+    for i in (p1, p2):
+        i.scatter(X, y, marker='x', c='r')
+        i.set_title('Profit distribution')
+        i.set_xlabel('Population of City in 10,000s')
+        i.set_ylabel('Profit in $10,000s')
 
     # Manipulate the dataset, add ones to its left
     X = hstack((ones(shape=(m, 1)), X))
-    theta = zeros(shape=(n, 1))
+    theta = zeros(shape=(2, 1))
 
     # Parameters for gradient descent
-    iter = 1500
+    iteration = 1500
     alpha = 0.01
+    theta, J_history = gradient_descent(X, y, theta, alpha, iteration)
 
-    theta, J_history = gradient_descent(X, y, theta, alpha, iter)
+    # Figure3: Monitor of cost function value
     p3.set_title('Cost function value history')
     p3.set_xlabel('Iteration number')
     p3.set_ylabel('Cost function value')
-    p3.plot(range(iter), J_history)
+    p3.plot(range(iteration), J_history)
     p3.scatter(10, J_history[10], marker='o', color='k')
     p3.scatter(100, J_history[100], marker='o', color='k')
     p3.scatter(1000, J_history[1000], marker='o', color='k')
 
-    pred = X.dot(theta).flatten()
-    p1.plot(X[:, 1:], pred)
+    # Figure1: Leverage gradient descent
+    pred = X.dot(theta)
+    p1.plot(X[:, 1], pred)
     p1.scatter(3.5, dot([1, 3.5], theta), marker='o', color='k')
     p1.scatter(7, dot([1, 7], theta), marker='o', color='k')
 
-    # Leverage linear regression model in sklearn package
+    # Figure2: Leverage linear regression model in sklearn package
     lr = linear_model.LinearRegression()
     lr.fit(X, y)
-    p2.plot(X[:, 1:], lr.predict(X))
+    p2.plot(X[:, 1], lr.predict(X))
     p2.scatter(3.5, lr.predict([[1, 3.5]]), marker='o', color='k')
     p2.scatter(7, lr.predict([[1, 7]]), marker='o', color='k')
 
+    # Figure4: Contour figure with respect to theta_0 and theta_1
     # Grid over which we will calculate J
     theta0_vals = linspace(-10, 10, 100)
     theta1_vals = linspace(-1, 4, 100)
@@ -75,12 +78,12 @@ def main():
     # Contour plot
     J_vals = J_vals.T
     # Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-    # print J_vals
     p4.contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
     p4.set_title('Contour, showing minimum')
     p4.set_xlabel('theta_0')
     p4.set_ylabel('theta_1')
     p4.scatter(theta[0][0], theta[1][0], marker='x', color='r')
+
     show()
 
 
