37a367cfc7a053c9c38cb4145c9b219620c475a1
“AvalancheSiqi” <wusiqi.china@gmail.com>
2016-04-21 20:57:37 +0800

    finish logistic regression

--- stanford_ml/LogisticRegression.py
+++ stanford_ml/LogisticRegression.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pylab import show, figure
+from pylab import show, figure, subplot
 from sklearn import linear_model
 from scipy import optimize
 
@@ -15,56 +15,68 @@ def main():
     y = dataset[:, n-1].reshape(m, 1)
 
     # Prepare the canvas
-    fig = figure(figsize=(14, 6), dpi=100)
-    p1 = fig.add_subplot(121)
-    # p2 = fig.add_subplot(122)
+    fig = figure(figsize=(14, 7), dpi=100)
+    # sklearn logistic regression
+    p1 = subplot(221)
+    # Gradient descent by fmin_bfgs
+    p2 = subplot(222)
+    # Gradient descent by iteration
+    p3 = subplot(223)
+    # Monitor of cost function value by iteration
+    p4 = subplot(224)
 
-    # Plot all points
     pos = np.where(y == 1)
     neg = np.where(y == 0)
-    pos_points = p1.scatter(X[pos[0]].T[0], X[pos[0]].T[1], marker='o', c='y')
-    neg_points = p1.scatter(X[neg[0]].T[0], X[neg[0]].T[1], marker='x', c='k')
-    p1.set_xlabel('Exam1 score')
-    p1.set_ylabel('Exam2 score')
-    p1.legend([pos_points, neg_points], ['Admitted', 'Not Admitted'])
+    # Plot all points
+    p1.set_title("sklearn logistic regression")
+    p1.set_title("Gradient descent by fmin_bfgs")
+    p1.set_title("Gradient descent by iteration")
+    for p in (p1, p2, p3):
+        pos_points = p.scatter(X[pos[0]].T[0], X[pos[0]].T[1], marker='o', c='y')
+        neg_points = p.scatter(X[neg[0]].T[0], X[neg[0]].T[1], marker='x', c='k')
+        p.set_xlabel('Exam1 score')
+        p.set_ylabel('Exam2 score')
+        p.legend([pos_points, neg_points], ['Admitted', 'Not Admitted'])
 
     # Add ones to its left
     X = np.hstack((np.ones(shape=(m, 1)), X))
-    theta = np.ones(shape=(n, 1))
 
-    # Parameters for gradient descent
-    # iteration = 1000000
-    # alpha = 0.001
-    # theta, J_history = gradient_descent(X, y, theta, alpha, iteration)
-    # print 'Cost at initial theta (zeros): %f'%J_history[0]
+    # Figure1: Leverage logistic regression model in sklearn package
+    # for some reasons it doesn't plot correctly, -3 to theta_0 shift will make it more sense, unknown reason
+    lr = linear_model.LogisticRegression()
+    lr.fit(X, y.ravel())
+    # Accuracy rate for sklearn logistic regression and plot in figure
+    acc1 = (lr.predict(X) == y.ravel()).astype(int).mean()*100
+    print 'sklearn logistic regression train accuracy: %.4f\n' % acc1
+    plot_db_points(p1, X, lr.coef_.ravel().reshape(n, 1))
 
-    theta = optimize.fmin_bfgs(compute_cost, theta, fprime=grad, args=(X, y))
+    # Figure2: Leverage gradient descent by fmin_bfgs
+    theta2 = np.ones(shape=(n, 1))
+    theta2 = optimize.fmin_bfgs(compute_cost, theta2, fprime=grad, args=(X, y))
+    # Accuracy rate for fmin_bfgs gradient descent and plot in figure
+    acc2 = (((sigmoid(np.dot(X, theta2)) >= 0.5).astype(int) == y.ravel()).astype(int).mean()*100)
+    print 'fmin_bfgs gradient descent train accuracy: %.4f\n' % acc2
+    plot_db_points(p2, X, theta2.reshape(n, 1))
 
-    print 'Gradient at final theta:'
-    print theta
+    # Figure3: Leverage gradient descent by iteration
+    theta3 = np.ones(shape=(n, 1))
+    iteration = 1000000
+    alpha = 0.001
+    theta3, J_history = gradient_descent(X, y, theta3, alpha, iteration)
+    # Accuracy rate for iteration gradient descent and plot in figure
+    acc3 = (((sigmoid(np.dot(X, theta3))>=0.5).astype(int) == y.ravel()).astype(int).mean()*100)
+    print 'iteration gradient descent train accuracy: %.4f\n' % acc3
+    plot_db_points(p3, X, theta3.reshape(n, 1))
 
-    # Figure2: Leverage logistic regression model in sklearn package
-    # lr = linear_model.LogisticRegression()
-    # lr.fit(X, y.ravel())
-    print 'For a student with scores 45 and 85, we predict an admission probability of %f' % sigmoid(np.dot([[1, 45, 85]], theta))
-    print 'For a student with scores 15 and 45, we predict an admission probability of %f' % sigmoid(np.dot([[1, 15, 45]], theta))
-    print 'For a student with scores 60 and 60, we predict an admission probability of %f' % sigmoid(np.dot([[1, 60, 60]], theta))
-
-    acc = (((sigmoid(np.dot(X, theta))>=0.5).astype(int) == y.ravel()).astype(int).mean()*100)
-    print 'Train Accuracy: %.4f\n' % acc
-
-    a, b = plot_db_points(X, theta.reshape(n, 1))
-    p1.plot(a, b)
-
-    # Figure2: Monitor of cost function value
-    # p2.set_title('Cost function value history')
-    # p2.set_xlabel('Iteration number')
-    # p2.set_ylabel('Cost function value')
-    # p2.plot(range(iteration), J_history)
-    # points = [100, 1000, 10000, 100000, 1000000, 5000000, 8000000]
-    # p2.scatter(points, J_history[points], marker='o', color='k')
-    # for x, y in zip(points, J_history[points]):
-    #     p2.annotate(y, xy=(x, y), ha='left', va='bottom')
+    # Figure4: Monitor of cost function value
+    p4.set_xlabel('Iteration number')
+    p4.set_ylabel('Cost function value')
+    p4.set_xlim([-100000, 1100000])
+    p4.plot(range(iteration), J_history)
+    points = [1000, 100000, 900000]
+    p4.scatter(points, J_history[points], marker='o', color='k')
+    for x, y in zip(points, J_history[points]):
+        p4.annotate(y, xy=(x, y), ha='left', va='bottom')
 
     show()
 
@@ -102,36 +114,40 @@ def gradient_descent(X, y, theta, alpha, iteration):
     :param iteration: iteration number
     :return: hypothesis / history list of cost function value
     '''
-    m = y.size
+    n = X.shape[1]
     J_history = np.zeros(shape=(iteration, 1))
 
     for i in range(iteration):
-        pred = sigmoid(np.dot(X, theta))
-        errors = np.dot(X.T, (pred - y))
-        theta = theta - 1.0*alpha/m*errors
-        J_history[i, 0] = compute_cost(X, y, theta)
+        theta = theta - alpha*np.array(grad(theta, X, y)).reshape(n, 1)
+        J_history[i, 0] = compute_cost(theta, X, y)
 
     return theta, J_history
 
 
 def grad(theta, X, y):
-    pred = sigmoid(np.dot(X, theta)).reshape(y.size, 1)
+    '''
+    gradient for a single example
+    '''
+    m = y.size
+    pred = sigmoid(np.dot(X, theta)).reshape(m, 1)
     error = np.dot(X.T, (pred - y))
-    return (error / y.size).flatten()
+    return (error / m).flatten()
 
 
 # Plot decision boundary two end points
-def plot_db_points(X, theta):
+def plot_db_points(fig, X, theta):
     '''
     Plot decision boundary two end points
+    :param fig: current figure canvas
     :param X: training matrix
     :param theta: final hypothesis
     :return: two end points that exist in drawing space
     '''
     if X.shape[1] <= 3:
-        plot_x = [min(X[:, 2])-2, max(X[:, 2])+2]
+        plot_x = [min(X[:, 2]-2), max(X[:, 2]+2)]
         plot_y = (-1.0 / theta[2]) * (theta[1]*plot_x+theta[0])
-    return plot_x, plot_y
+        fig.plot(plot_x, plot_y)
+    return
 
 
 if __name__ == '__main__':
