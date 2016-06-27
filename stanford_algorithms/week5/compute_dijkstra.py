4087e064068a61e3cda29e15e144e6ff2dd4ecf5
“avalanchesiqi” <wusiqi.china@gmail.com>
2016-06-27 18:30:38 +0800

    algo dijkstra code refine

--- stanford_algorithms/week5/compute_dijkstra.py
+++ stanford_algorithms/week5/compute_dijkstra.py
@@ -14,17 +14,14 @@ Created by Siqi Wu on Feb 21 2015.
 
 import sys
 import os
-from collections import defaultdict
-import copy
 
 def shortest_link(links, distance):
     min_dist = 1000000
     for k, v in links.items():
         if (v + distance[k[0]]) < min_dist:
-            source = k[0]
             next = k[1]
             min_dist = v + distance[k[0]]
-    return source, next, min_dist
+    return next, min_dist
 
 def update(links, next, explored, nodes, graph):
     explored.add(next)
@@ -32,28 +29,24 @@ def update(links, next, explored, nodes, graph):
     for k, v in links.items():
         if k[0] in explored and k[1] in explored:
             del links[k]
-    for dst in graph[next]:
-        if dst not in explored:
-            links[(next, dst)] = graph[next][dst]
+    for n2 in graph[next].keys():
+        if n2 not in explored:
+            links[(next, n2)] = graph[next][n2]
+    return
 
 def dijkstra_algo(graph, nodes, src):
     distance = {}
-    path = defaultdict(list)
     explored = set([])
     links = {}
     distance[src] = 0
-    path[src] = []
     explored.add(src)
     nodes.remove(src)
-    for dst in graph[src]:
-        links[(src, dst)] = graph[src][dst]
+    for n2 in graph[src].keys():
+        links[(src, n2)] = graph[src][n2]
     while(nodes != set([])):
-        source, next, min_dist = shortest_link(links, distance)
-        #print 'source: %s, next: %s, min_dist: %d' % (source, next, min_dist)
+        next, min_dist = shortest_link(links, distance)
+        #print 'source: %s, next: %s, min_dist: %d' % (src, next, min_dist)
         distance[next] = min_dist
-        temp = copy.deepcopy(path[source])
-        temp.append(next)
-        path[next] = temp
         update(links, next, explored, nodes, graph)
     return distance
 
@@ -63,23 +56,23 @@ def parse_graph(input_file):
     for line in input_file:
         adjacent = {}
         fields = line.rstrip().split()
-        vertex = fields.pop(0)
-        nodes.add(vertex)
+        n1 = int(fields.pop(0))
+        nodes.add(n1)
         for items in fields:
-            dst, dist = items.split(',')
-            adjacent[dst] = int(dist)
-        graph[vertex] = adjacent
+            n2, dist = items.split(',')
+            adjacent[int(n2)] = int(dist)
+        graph[n1] = adjacent
     return graph, nodes
 
 def main():
     input_file = open(sys.argv[1], 'r')
     graph, nodes = parse_graph(input_file)
-    shortest_dists = dijkstra_algo(graph, nodes, '1')
+    shortest_dists = dijkstra_algo(graph, nodes, 1)
     desired = [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]
     output = []
     for i in desired:
-        output.append(str(shortest_dists[str(i)]))
-    print ','.join(output)
+        output.append(shortest_dists[i])
+    print output
 
 if __name__ == '__main__':
     main()
